f07a3a1ed2066d376b7eebd759b75f406152eb5c
YOU PAN <sunnypanyou@gmail.com>
2023-02-24 14:35:19 -0500

    using get /

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'lists#index'
+  get '/', to: 'lists#index'
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
