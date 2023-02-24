60558da75455a079860aa72915ed7bae02505ab0
YOU PAN <sunnypanyou@gmail.com>
2023-02-24 12:15:48 -0500

    remember to do git push each change on the files

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
-  end
+end
